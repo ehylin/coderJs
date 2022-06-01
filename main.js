8cafa6e0300334359baae0552564cf647c056103
Ehylin <ehylinolezjua@gmail.com>
2022-06-01 18:33:12 -0400

    crear un algoritmo usando un ciclo

---
+++ main.js
@@ -0,0 +1,24 @@
+//Utilizando un bucle, mostrar la suma y la media de los numeros introducidos 
+// hasta introducir un numero negativo y ahi mostrar el resultado
+
+var suma = 0;
+var contador = 0;
+
+
+do{
+    var numero = parseInt(prompt('Introduce numeros hasta que metas numeros negativos', 0))
+    if(isNaN(numero)){
+        numero = 0;
+    }else if(numero >= 0){
+        suma = suma + numero;
+
+        contador++;
+    }
+     console.log(suma);
+     console.log(contador);
+
+}while(numero >= 0)
+
+
+alert('la suma de todos los numero es: ' + suma);
+alert('La media de todos los numeros es: ' + (suma/contador));
