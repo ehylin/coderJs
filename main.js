9ee9ddb8320ece4344763c6664451578b0408d61
Ehylin <ehylinolezjua@gmail.com>
2022-06-07 18:30:40 -0400

    funciones

--- main.js
+++ main.js
@@ -1,82 +1,40 @@
-//Desafio coderHouse
-//Juego piedra papel y tijera contra la maquina
-//Resultado en la consola 
+"use strict";
 
-const repetitions = prompt(
-    "Ingrese la cantidad de veces que deesea quie se repita el juego"
-  );
-  const repetitionsQty = Number.parseInt(repetitions);
-  
-  console.log(`El usuario quiere que el juego se repita ${repetitionsQty} veces`);
-  
-  for (let count = 1; count <= repetitionsQty; count = count + 1) {
-    //pedir una respuesta
-    const userMove = prompt(
-      "Ingrese su jugada, debe ser: Piedra, Papel o Tijera"
-    );
-  
-    const randomMoveIndex = Math.floor(Math.random() * (4 - 1)) + 1;
-    // 1 o 2 o 3;
-    let machineMove = "";
-  
-    if (randomMoveIndex === 1) {
-      machineMove = "Piedra";
-    } else if (randomMoveIndex === 2) {
-      machineMove = "Papel";
-    } else if (randomMoveIndex === 3) {
-      machineMove = "Tijera";
-    } else {
-      console.error(`La opcion randomMoveIndex no es valida: ${randomMoveIndex}`);
-    }
+/**
+ * Funcion que al invocarla, le pregunta al usuario por el numero de repeticiones
+ * @returns {number|null}
+ */
+function askForLimitNumber() {
+ 
+  const limit = Number.parseInt(prompt("Ingrese el numero"));
 
-  
-    /**
-     * alternativa con switch-case
-     */
-    // switch (randomMoveIndex) {
-    //   case 1:
-    //     randomMoveText = "Piedra";
-    //     break;
-    //   case 2:
-    //     randomMoveText = "Papel";
-    //     break;
-    //   case 3:
-    //     randomMoveText = "Tijera";
-    //     break;
-    // }
-  
-    let winner = ""; // Machine - User - Both - Unknown
-  
-    if (machineMove === "Piedra") {
-      if (userMove === "Papel") {
-        winner = "User";
-      } else if (userMove === "Tijera") {
-        winner = "Machine";
-      } else {
-        winner = "Both";
-      }
-    } else if (machineMove === "Papel") {
-      if (userMove === "Tijera") {
-        winner = "User";
-      } else if (userMove === "Piedra") {
-        winner = "Machine";
-      } else {
-        winner = "Both";
-      }
-    } else if (machineMove === "Tijera") {
-      if (userMove === "Piedra") {
-        winner = "User";
-      } else if (userMove === "Papel") {
-        winner = "Machine";
-      } else {
-        winner = "Both";
-      }
-    } else {
-      winner = "Unknown";
-    }
-  
-    // mostrar el resultado
-    console.log({ userMove, machineMove });
-    console.log(`El ganador es: ${winner}`);
+  if (limit >= 1 && limit <= 20) {
+    console.log("el numero esta dentro del rango");
+    return limit;
+  } else {
+    console.log("el numero esta fuera de rango");
+    return null;
   }
-  
+}
+
+
+const limitNumber = askForLimitNumber(); 
+
+
+for (let i = 1; i <= limitNumber; i++) {
+  console.log(`${i} x 10 = ${i * 10}`);
+}
+
+// 5 => 5 x 4 x 3 x 2 x 1
+// 6 => 6 x 5 x 4 x 3 x 2 x 1
+
+/**
+ * Calcular el factorial de todos los numeros enteros desde 1 a N donde N es igual a limitNumber
+ */
+for (let factoryNumber = 1; factoryNumber <= limitNumber; factoryNumber++) {
+  let result = 1;
+  for (let multiplier = 1; multiplier <= factoryNumber; multiplier++) {
+    result = result * multiplier;
+  }
+  console.log(`El factorial de ${factoryNumber} puede ser ${result}`);
+}
